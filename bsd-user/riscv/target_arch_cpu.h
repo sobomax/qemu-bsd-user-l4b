276584136351154c5895b211aa2c7130712bfcb7
Jessica Clarke <jrtc27@jrtc27.com>
2024-05-31 00:24:06 +0100

    bsd-user: Fix riscv's target_cpu_loop
    Old versions of this code wrote to info's members, but this was split
    out into individual variables. However, the condition for when to call
    force_sig_fault still checks the struct member, which is uninitialised.
    Change it to check the actual variable in use, and delete the now unused
    info variable.
    
    Signed-off-by: Jessica Clarke <jrtc27@jrtc27.com>

--- bsd-user/riscv/target_arch_cpu.h
+++ bsd-user/riscv/target_arch_cpu.h
@@ -40,7 +40,6 @@ static inline void target_cpu_loop(CPURISCVState *env)
 {
     CPUState *cs = env_cpu(env);
     int trapnr;
-    target_siginfo_t info;
     abi_long ret;
     unsigned int syscall_num;
     int32_t signo, code;
@@ -123,7 +122,7 @@ static inline void target_cpu_loop(CPURISCVState *env)
             abort();
         }
 
-        if (info.si_signo) {
+        if (signo) {
             force_sig_fault(signo, code, env->pc);
         }
 
