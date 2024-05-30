5eb608e56a10bda0b11f7d05e0ba47298ca83967
Jessica Clarke <jrtc27@jrtc27.com>
2024-05-31 00:32:52 +0100

    bsd-user: Correctly handle NULL msg_control for sendmsg
    FreeBSD's sendmsg requires msg_controllen to be valid, i.e. at least one
    message, if msg_control is non-NULL, with no special casing for 0. This
    is needed for pkg < 1.21, which used socketpair + sendmsg for its worker
    processes for creating a local repository. For recvmsg this might not
    matter but we might as well be careful and faithfully mirror the
    pointer's NULL-ness still.
    
    Signed-off-by: Jessica Clarke <jrtc27@jrtc27.com>

--- bsd-user/freebsd/os-socket.h
+++ bsd-user/freebsd/os-socket.h
@@ -61,8 +61,12 @@ static abi_long do_sendrecvmsg_locked(int fd, struct target_msghdr *msgp,
         msg.msg_namelen = 0;
     }
     msg.msg_controllen = 2 * tswap32(msgp->msg_controllen);
-    msg.msg_control = alloca(msg.msg_controllen);
-    memset(msg.msg_control, 0, msg.msg_controllen);
+    if (msgp->msg_control) {
+        msg.msg_control = alloca(msg.msg_controllen);
+        memset(msg.msg_control, 0, msg.msg_controllen);
+    } else {
+        msg.msg_control = NULL;
+    }
 
     msg.msg_flags = tswap32(msgp->msg_flags);
 
@@ -87,7 +91,11 @@ static abi_long do_sendrecvmsg_locked(int fd, struct target_msghdr *msgp,
     msg.msg_iov = vec;
 
     if (send) {
-        ret = t2h_freebsd_cmsg(&msg, msgp);
+        if (msg.msg_control != NULL) {
+            ret = t2h_freebsd_cmsg(&msg, msgp);
+        } else {
+            ret = 0;
+        }
         if (ret == 0) {
             ret = get_errno(safe_sendmsg(fd, &msg, flags));
         }
@@ -95,7 +103,11 @@ static abi_long do_sendrecvmsg_locked(int fd, struct target_msghdr *msgp,
         ret = get_errno(safe_recvmsg(fd, &msg, flags));
         if (!is_error(ret)) {
             len = ret;
-            ret = h2t_freebsd_cmsg(msgp, &msg);
+            if (msg.msg_control != NULL) {
+                ret = h2t_freebsd_cmsg(msgp, &msg);
+            } else {
+                ret = 0;
+            }
             if (!is_error(ret)) {
                 msgp->msg_namelen = tswap32(msg.msg_namelen);
                 msgp->msg_flags = tswap32(msg.msg_flags);
