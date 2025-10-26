9be79f307575d73b411565d2218dd3325a41f60b
Maksym Sobolyev <sobomax@sippysoft.com>
2025-10-25 20:39:44 -0700

    Fix build on linux/ppc64le.

--- bsd-user/host/ppc/host-signal.h
+++ bsd-user/host/ppc/host-signal.h
@@ -9,26 +9,40 @@
 #ifndef PPC_HOST_SIGNAL_H
 #define PPC_HOST_SIGNAL_H
 
+#if !defined(__linux__)
 #include <machine/trap.h>
 #include <machine/spr.h>
+#endif
 
 static inline uintptr_t host_signal_pc(ucontext_t *uc)
 {
+#if !defined(__linux__)
     return uc->uc_mcontext.mc_ssr0;
+#else
+    abort();
+#endif
 }
 
 static inline void host_signal_set_pc(ucontext_t *uc, uintptr_t pc)
 {
+#if !defined(__linux__)
     uc->uc_mcontext.mc_ssr0 = pc;
+#else
+    abort();
+#endif
 }
 
 static inline bool host_signal_write(siginfo_t *info, ucontext_t *uc)
 {
+#if !defined(__linux__)
     /*
      * Compare with start of trap_pfault() in sys/powerpc/trap.c
      */
     return uc->uc_mcontext.mc_exc != EXC_ISI &&
         (uc->uc_mcontext.mc_dsisr & DSISR_STORE);
+#else
+    abort();
+#endif
 }
 
 #endif
